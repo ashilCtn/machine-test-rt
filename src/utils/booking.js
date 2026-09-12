2f4cfcc385ab52e702e82af25d66dd1fafe1390c
ashilCtn <pashil364@gmail.com>
2026-09-12 19:30:21 +0530

    project structuring

---
+++ src/utils/booking.js
@@ -0,0 +1,32 @@
+export const rooms = [
+  {
+    code: "R101",
+    type: "Deluxe Room",
+    price: 3500,
+    maxGuests: 2,
+  },
+  {
+    code: "R102",
+    type: "Deluxe Room",
+    price: 3500,
+    maxGuests: 2,
+  },
+  {
+    code: "R201",
+    type: "Executive Suite",
+    price: 5800,
+    maxGuests: 3,
+  },
+  {
+    code: "R202",
+    type: "Executive Suite",
+    price: 5800,
+    maxGuests: 3,
+  },
+  {
+    code: "R301",
+    type: "Family Room",
+    price: 4200,
+    maxGuests: 4,
+  },
+];
